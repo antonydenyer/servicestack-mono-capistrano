a862fbf15d076537c2eedafe3f6c67b0831dc554
Antony Denyer <antonydenyer@gmail.com>
2012-12-19 16:52:26 +0000

    added user

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,6 @@ ssh_options[:keys] = %w('~/.ssh/*.pub')
 set :application, "service stack deployed with capistrano running on mono"
 set :deploy_to, "~/www" 
 
-role :app, "10.112.6.41"
+role :app, "MACHINE_NAME"
 
-set :user, "dev"
+set :user, "USER_NAME"
