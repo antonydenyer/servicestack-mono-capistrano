c56191f0c5432308808f1850cb25f09d75e1bfa1
Antony Denyer <antonydenyer@gmail.com>
2012-12-19 16:11:37 +0000

    tidy up to use common namespaces

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,5 +5,6 @@ ssh_options[:keys] = %w('~/.ssh/*.pub')
 set :application, "service stack deployed with capistrano running on mono"
 set :deploy_to, "~/www" 
 
-role :app, "192.168.1.105"                      
+role :app, "10.112.6.41"
 
+set :user, "dev"
